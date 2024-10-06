7c1d81f9637618a30ecf508a25c01dfb722b812f
Sohaib Aamir <103496288+SohaibAamir28@users.noreply.github.com>
2024-10-07 03:04:49 +0500

    Version 1.3

--- next.config.js
+++ next.config.js
@@ -5,19 +5,24 @@ const CopyPlugin = require('copy-webpack-plugin');
 
 const nextConfig = {
   reactStrictMode: true,
+  // Enabling the experimental Webpack build worker
+  experimental: {
+    webpackBuildWorker: true,
+  },
   webpack: (config) => {
+    // Add CopyPlugin to copy images from Leaflet's node_modules to public folder
     config.plugins.push(
       new CopyPlugin({
         patterns: [
           {
             from: 'node_modules/leaflet/dist/images',
-            to: path.resolve(__dirname, 'public', 'leaflet', 'images')
+            to: path.resolve(__dirname, 'public', 'leaflet', 'images'),
           },
         ],
-      }),
-    )
-    return config
-  }
-}
+      })
+    );
+    return config;
+  },
+};
 
 module.exports = nextConfig;
